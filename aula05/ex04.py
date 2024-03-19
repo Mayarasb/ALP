7849dfcb5689703847d1ad530b2e9f74ad42bfda
Mayara Barros <mayarasbarros7@gmail.com>
2024-03-18 22:39:08 -0300

    Exercicios aula 05

--- aula05/ex04.py
+++ aula05/ex04.py
@@ -2,12 +2,11 @@ a = input("Digite o primeiro lado do triângulo: ")
 b = input("Digite o segundo lado do triângulo: ")
 c = input("Digite o terceiro lado do triângulo: ")
 if ((a + b) < c) or ((b + c) < a) or ((a + c ) < b):
-    print("Pode formar um triangulo")
+    print("Não pode formar um triangulo")
+else:
     if (a == b == c):
         print("Forma um triangulo equilátero")
     elif (a != b != c):
         print("Forma um trangulo escaleno")
     else:
         print("Forma um triangulo isósceles")
-else:
-    print("Não pode forma um triângulo")
